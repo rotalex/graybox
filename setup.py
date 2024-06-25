b3da98dd73138da466b9e3c782e981ee2c8c4499
Alexandru Rotaru <rotarualexandruandrei94@gmail.com>
2024-06-25 17:32:52 +0200

    Create setup.py for the package

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='graybox',
+    version='0.0.0',
+    description='Paving the way between black-box and white-box modeling.',
+    url='https://github.com/rotalex/graybox',
+    author='Alexandru-Andrei Rotaru',
+    author_email='alexandru@graybx.com',
+    license='BSD 2-clause',
+    packages=[
+        'graybox'
+    ],
+    install_requires=[
+        'mpi4py>=2.0',
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License', 
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
